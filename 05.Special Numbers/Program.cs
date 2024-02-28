3431a7abf0a7d376b2b5010504d462cf7ecea90c
Hristiyan Stefanov Velev <145477797+hristiyan96@users.noreply.github.com>
2024-02-28 17:43:18 +0200

    Add files via upload

---
+++ 05.Special Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+
+
+for (int number = 1; number <= n; number++)
+{
+    bool isSpecial = IsSpecialNumber(number);
+    Console.WriteLine($"{number} -> {isSpecial}");
+}
+static bool IsSpecialNumber(int number)
+{
+    int sum = 0;
+    while(number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum == 5 || sum == 7 || sum == 11;
+}
+
+
---
+++ 06.Reversed Chars/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+char a = Console.ReadLine()[0];
+char b = Console.ReadLine()[0];
+char c = Console.ReadLine()[0];
+Console.WriteLine($"{c} {b} {a}");
+
